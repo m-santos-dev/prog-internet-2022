2ebd2570a53b0a872fca0ed4b59dd71968b7681a
Marcelo Olmedo <marceloolmedo93@hotmail.com>
2022-08-27 13:03:19 -0300

    Conteudo aula 15, mario e calculadora

--- AULA 11/Aula11-game/assets/js/codigo.js
+++ AULA 11/Aula11-game/assets/js/codigo.js
@@ -2,9 +2,30 @@ const mario = document.querySelector("#mario");
 const cano = document.querySelector("#pipe");
 
 const loop = setInterval(() => {
-    if (cano.offsetLeft <= 98){
+    const canoPosicao = cano.offsetLeft;
+    const modal = document.querySelector(".modal");
+    const marioPosicao = window.getComputedStyle(mario).bottom.replace("px","");
+    
+    const btnsReset = document.querySelectorAll(".clReset");
+    
+    Array.from(btnsReset).forEach(btn => {
+        btn.addEventListener("click", ()=>{
+            document.location.reload();
+        })
+    })
+    console.log(marioPosicao)
+    if (cano.offsetLeft >= -65 && cano.offsetLeft <= 98 && marioPosicao < 105){
         cano.style.animation = "none"
-        cano.style.offsetLeft = offsetLeft
+        cano.style.left = `${canoPosicao}px`;
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosicao}px`; 
+        mario.src = "assets/img/game-over.png";
+        mario.style.height = "85px";
+        mario.style.marginLeft = "40px";
+        // cancelando o setInverval para não ficar consumindo
+        clearInterval (loop);
+        // mostrando o modal
+        modal.classList.add("show");
     }
 }, 100);
 
