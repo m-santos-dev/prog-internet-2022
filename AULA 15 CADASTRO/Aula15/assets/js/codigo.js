7f689088c931d3ade0105c5b5dd4fbb710f68c78
Marcelo Olmedo <marceloolmedo93@hotmail.com>
2022-09-03 12:59:44 -0300

    Aula 16

--- AULA 15 CADASTRO/Aula15/assets/js/codigo.js
+++ AULA 15 CADASTRO/Aula15/assets/js/codigo.js
@@ -28,41 +28,7 @@ btnPesqCep.addEventListener("click",()=>{
 // Criando a função para testar os dados de envio do formulário
 function testarFormulario(evento,frm){
   let ret = false;
-  // evento.preventDefault(); para evitar o envio do formulario se as linhas abaixo estiverem com erro
   // fazer os testes aqui
-
-  const nome = document.querySelector("#nome");
-  const msgErros = document.querySelector(".msgErros");
-
-  // limpando as mensagems de rro
-  msgErros.innerHTML = "";
-  Array.from(document.querySelectorAll("show")).forEach(it=>{
-    it.classList.remove("show")
-  })
-
-  // campo msgErros vai ser preenchido conforme os erros
-  // if(nome.value=="") document.querySelector("#erroNome").classList.add("show");
-
-  // Podemos testar pelo formulario que é passado na funcao
-  if(frm["nome"].value=="")
-    msgErros.innerHTML += "<li>Campo Nome deve ser preenchido!</li>"
-  if (frm["cpf"].value=="")
-    msgErros.innerHTML += "<li>Campo CPF deve ser preenchido!</li>"
-  if(frm["nascimento"].value=="")
-    msgErros.innerHTML += "<li>Campo Data de Nascimento deve ser preenchido!</li>"
-  if(frm["sexo"].value=="")
-    msgErros.innerHTML += "<li>Campo Sexo deve ser marcado corretamente!</li>"
-  if(frm["telefone"].value=="")
-    msgErros.innerHTML += "<li>Campo Telefone deve ser preenchido!</li>"
-  if(frm["celular"].value=="")
-    msgErros.innerHTML += "<li>Campo Celular deve ser preenchido!</li>"
-  if(frm["email"].value=="")
-    msgErros.innerHTML += "<li>Campo E-mail deve ser preenchido!</li>"
-  if(frm["cep"].value=="")
-    msgErros.innerHTML += "<li>Pelo amor de Deus, preenche alguma coisa!</li>"
-  
-    if(msgErros.innerHTML=="") ret=true;
-  
 
   //fim do código
   if (!ret) 
